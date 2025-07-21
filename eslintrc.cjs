56e68be703af763405bd8ab5ed164f716252a56d
pumpkynn <19198402685@163.com>
2025-07-21 16:13:38 +0800

    chore: 项目初始化

---
+++ eslintrc.cjs
@@ -0,0 +1,45 @@
+// @see https://eslint.bootcss.com/docs/rules/
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-var': 'error',
+    'no-multiple-empty-lines': ['warn', { max: 1 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unexpected-multiline': 'error',
+    'no-useless-escape': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/semi': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/script-setup-uses-vars': 'error',
+    'vue/no-mutating-props': 'off',
+    'vue/attribute-hyphenation': 'off',
+  },
+}
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>App</div>
+  <el-button type="primary" size="default" >主要按钮
+  </el-button>
+
+</template>
+
+<style scoped>
+</style>
